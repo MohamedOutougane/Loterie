f87613394038787edf677d84e4257b23f6e88cb6
Outougane Mohamed <outougane.moh@gmail.com>
2022-11-28 12:07:21 +0100

    creation of dataLayerDbContext

---
+++ DataLayer/DataLayerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DataLayer.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class DataLayerDbContext : DbContext
+    {
+        public DataLayerDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Sessions> Sessions { get; set; }
+        public DbSet<Tickets> Tickets { get; set; }
+    }
+}
