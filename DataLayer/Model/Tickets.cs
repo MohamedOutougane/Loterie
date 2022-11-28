780668ed39e630465e221fdbc680166922606bea
Outougane Mohamed <outougane.moh@gmail.com>
2022-11-28 11:36:53 +0100

    creation of tickets class

---
+++ DataLayer/Model/Tickets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Model
+{
+    internal class Tickets
+    {
+        [Key]
+        public string shortGUID { get; set; }
+        public string Combination { get; set; }
+        public int idSessions { get; set; }
+    }
+}
