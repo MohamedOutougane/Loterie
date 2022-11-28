6e6aded90cb18032df3a1ea05a4de1f72698e523
Outougane Mohamed <outougane.moh@gmail.com>
2022-11-28 11:36:42 +0100

    making my "partie" class in english

---
+++ DataLayer/Model/Sessions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Model
+{
+    internal class Sessions
+    {
+        [Key]
+        public int idSession { get; set; }
+        public string Draw { get; set; }
+        public int Jackpot { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
