781ebd5b8d15587d1ba7fd8966a790f94a5c284d
Outougane Mohamed <outougane.moh@gmail.com>
2022-11-28 12:31:04 +0100

    made my migration

---
+++ DataLayer/Migrations/20221128112915_LoterieDB.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class LoterieDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Draw = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Jackpot = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    shortGUID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Combination = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SessionsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.shortGUID);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Sessions_SessionsId",
+                        column: x => x.SessionsId,
+                        principalTable: "Sessions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_SessionsId",
+                table: "Tickets",
+                column: "SessionsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+        }
+    }
+}
