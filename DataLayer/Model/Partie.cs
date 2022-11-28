1d387246aec9fa0295335ce4a8d5182577abf3c7
Outougane Mohamed <outougane.moh@gmail.com>
2022-11-28 11:27:24 +0100

    creation of Partie class

---
+++ DataLayer/Model/Partie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Model
+{
+    internal class Partie
+    {
+        public int idPartie { get; set; }
+        public string resultatTirage { get; set; }
+        public int Cagnotte { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
